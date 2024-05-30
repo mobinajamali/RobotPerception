ef11f2958a8d47d65bccff97f0a0783c28b3393f
mobinajamali <mobina.jamali.mj@gmail.com>
2024-05-30 03:32:01 +0000

    added ORB feature matching

--- src/wanted.py
+++ src/wanted.py
@@ -7,7 +7,10 @@ import numpy as np
 
 
 class LoadFeature(object):
-
+    """
+    detect and match features between camera feed 
+    and stored image 
+    """
     def __init__(self):
     
         self.sub = rospy.Subscriber("/camera/rgb/image_raw",Image,self.camera_callback)
@@ -22,88 +25,70 @@ class LoadFeature(object):
 
         image_1 = cv.imread('/home/user/catkin_ws/src/robot-perception/images/image.jpg',1)
         image_2 = cv_image
-        hog = cv.HOGDescriptor()
-        hog.setSVMDetector(cv.HOGDescriptor_getDefaultPeopleDetector())
         
+        # process images
         img_2 = cv.resize(cv_image,(700,500))
         image_2 = img_2
-
         gray_2 = cv.cvtColor(img_2, cv.COLOR_RGB2GRAY)
 
+        # people detection using HOG descriptor
+        hog = cv.HOGDescriptor()
+        hog.setSVMDetector(cv.HOGDescriptor_getDefaultPeopleDetector())
         boxes_2, weights_2 = hog.detectMultiScale(gray_2, winStride=(8,6) )
         boxes_2 = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes_2])
 
-
+        # iterate over detected bounding boxes, calculate the center of each bounding box 
+        # and draw a circle at the center, draw rectangles around detected people
         for (xA, yA, xB, yB) in boxes_2:
             
-            #Center in X 
             medX = xB - xA 
             xC = int(xA+(medX/2)) 
-
-            #Center in Y
             medY = yB - yA 
             yC = int(yA+(medY/2)) 
 
-            #Draw a circle in the center of the box 
             cv.circle(img_2,(xC,yC), 1, (0,255,255), -1)
-
-            # display the detected boxes in the colour picture
             cv.rectangle(img_2, (xA, yA), (xB, yB),(255, 255, 0), 2)
 
         gray_1 = cv.cvtColor(image_1, cv.COLOR_RGB2GRAY)
         gray_2 = cv.cvtColor(image_2, cv.COLOR_RGB2GRAY)
 
-        #Initialize the ORB Feature detector 
+        # ORB feature detection and matching initialization
         orb = cv.ORB_create(nfeatures = 1000)
 
-        #Make a copy of the original image to display the keypoints found by ORB
-        #This is just a representative
+        # make copies of the original images for displaying keypoints
         preview_1 = np.copy(image_1)
         preview_2 = np.copy(image_2)
-
-        #Create another copy to display points only
         dots = np.copy(image_1)
 
-        #Extract the keypoints from both images
+        # detect ORB keypoints and compute descriptors for the stored image and camera output
         train_keypoints, train_descriptor = orb.detectAndCompute(gray_1, None)
         test_keypoints, test_descriptor = orb.detectAndCompute(gray_2, None)
 
-        #Draw the found Keypoints of the main image
+        # draw Keypoints of the images
         cv.drawKeypoints(image_1, train_keypoints, preview_1, flags = cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
         cv.drawKeypoints(image_1, train_keypoints, dots, flags=2)
 
-        ################## MATCHER ##################
 
-        #Initialize the BruteForce Matcher
+        # initialize the BruteForce matcher and matches feature points between the two images
         bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck = True)
-
-        #Match the feature points from both images
         matches = bf.match(train_descriptor, test_descriptor)
 
-        #The matches with shorter distance are the ones we want.
-        matches = sorted(matches, key = lambda x : x.distance)
-
-        #Catch some of the matching points to draw           
+        # select the best matches based on distance
+        matches = sorted(matches, key = lambda x : x.distance)        
         good_matches = matches[:313] 
         
-        #Parse the feature points
+        # extract the matching keypoints coordinates
         train_points = np.float32([train_keypoints[m.queryIdx].pt for m in good_matches]).reshape(-1,1,2)
         test_points = np.float32([test_keypoints[m.trainIdx].pt for m in good_matches]).reshape(-1,1,2)
 
-        #Create a mask to catch the matching points 
+        # compute the homography matrix  and define the corners of the image 
+        # and transform to the prospective of camera
         M, mask = cv.findHomography(train_points, test_points, cv.RANSAC,5.0)
-
-        #Catch the width and height from the main image
         h,w = gray_1.shape[:2]
-
-        #Create a floating matrix for the new perspective
         pts = np.float32([[0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-
-        #Create the perspective in the result 
         dst = cv.perspectiveTransform(pts,M)
 
-
-        # Draw the points of the new perspective in the result image (This is considered the bounding box)
+        # draw the points of the new perspective in the result image 
         result = cv.polylines(image_2, [np.int32(dst)], True, (50,0,255),3, cv.LINE_AA)
 
 
